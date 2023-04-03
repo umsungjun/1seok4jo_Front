7c7ee1909ee5336407d80f6dedcfdff6f7913050
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-04-03 14:06:12 +0900

    Current file
    Current file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+function App() {
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/hello').then(response => setHello(response.data))
+  }, [])
+
+  return <div>프론트엔드 1석 4조</div>
+}
+
+export default App
