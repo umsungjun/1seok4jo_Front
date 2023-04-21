6e8382905a2999146d26163c814d8e860ecb0d6e
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-04-21 23:31:01 +0900

    Redux Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import {BrowserRouter} from 'react-router-dom'
 import Header from './Component/Header/Header'
 import PageNavigator from './PageNavigator'
+import {Provider} from 'react-redux'
+import {store} from './Store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <PageNavigator />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <PageNavigator />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
