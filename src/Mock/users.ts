dc9eb2ebc038dc69e4aedc351879db9747b73d41
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-04-24 22:24:00 +0900

    basicUser data

--- src/Mock/users.ts
+++ src/Mock/users.ts
@@ -1,14 +1,19 @@
-import {UserInterface} from '../Interface/interface'
+// import {UserInterface} from '../Interface/interface'
 
-export const users: UserInterface[] = [
-  {
-    email: 'kikI@naver.com', // 이메일
-    password: 'zero123', // 비밀번호
-    nickName: 'kikI', // 닉네임
-    myPage: {
-      ment: '안녕하세요 저는 바다를 좋아하는 여행가 입니다!', // 나를 소개하는 한문장
-      profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyVFUA_3HIX1imDdXcf51nRNyibm6cxC1FXw&usqp=CAU', // profile이미지
-      background: 'https://blog.kakaocdn.net/dn/d2gF6H/btqXTm0xV6r/RoCwErZI7yKZYRbybrAouk/img.jpg', // background 배너 이미지
-    },
-  },
-]
+// export const users: UserInterface[] = [
+//   {
+//     email: 'kikI@naver.com', // 이메일
+//     password: 'zero123', // 비밀번호
+//     nickName: 'kikI', // 닉네임
+//     myPage: {
+//       ment: '안녕하세요 저는 바다를 좋아하는 여행가 입니다!', // 나를 소개하는 한문장
+//       profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyVFUA_3HIX1imDdXcf51nRNyibm6cxC1FXw&usqp=CAU', // profile이미지
+//       background: 'https://blog.kakaocdn.net/dn/d2gF6H/btqXTm0xV6r/RoCwErZI7yKZYRbybrAouk/img.jpg', // background 배너 이미지
+//     },
+//   },
+// ]
+
+export const basicUser = {
+  profile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyVFUA_3HIX1imDdXcf51nRNyibm6cxC1FXw&usqp=CAU', // profile이미지
+  background: 'https://blog.kakaocdn.net/dn/d2gF6H/btqXTm0xV6r/RoCwErZI7yKZYRbybrAouk/img.jpg', // background 배너 이미지
+}
