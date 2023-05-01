39f80f41f8d598826868bbacbd34b8a167c208ed
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-05-01 23:43:06 +0900

    Fetch Mypage

--- src/Service/myPageService.ts
+++ src/Service/myPageService.ts
@@ -2,12 +2,29 @@ import axios from 'axios'
 
 const remote = axios.create()
 
-export const fetchMyPostApi = async () => {
-  const searchURL = `http://localhost:8080/api/search/getList/`
+export interface MyPostListType {
+  count: number
+  postResponseList: {
+    id: number
+    title: string
+    detail: string
+    location: string
+    hashtag: string
+    likeCount: number
+    startDate: string
+    endDate: string
+    baseUrl: string
+    storeFileUrl: string[]
+  }[]
+}
+
+export const fetchPostListApi = async (type: string, token: string) => {
+  const myPostListURL = `http://localhost:8080/api/member/post/${type}`
 
   const params = {
     method: 'GET',
-    url: `${searchURL}`,
+    url: `${myPostListURL}`,
+    headers: {Authorization: token},
   }
   const response = await remote(params)
 
