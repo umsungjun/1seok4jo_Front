39cc72acaae82d83bb4e7c148d917c871e038eed
Stella_Ha <sangyeeh@gmail.com>
2023-05-18 17:18:41 +0900

    :coffin: remove dead code
    remove dead code

--- src/Service/postWriteService.ts
+++ src/Service/postWriteService.ts
@@ -2,17 +2,14 @@ import axios from 'axios'
 
 const remote = axios.create()
 
+// 글 작성
 export const fetchPostWriteApi = async (formData: FormData, token: string) => {
   const postWriteURL = `http://localhost:8080/post`
   const headers = {
     'Content-Type': 'multipart/form-data',
     'Access-Control-Allow-Origin': '*',
-    Authorization: `Bearer ${token}`,
+    Authorization: token,
   }
-  // const data: {[key: string]: string} = {}
-  // formData.forEach((value, key) => {
-  //   data[key] = value
-  // })
   try {
     const response = await remote.post(postWriteURL, {headers})
     return response.data
